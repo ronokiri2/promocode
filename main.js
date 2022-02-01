b9714de43411a7fa8d85bd200c9e758ba32acf59
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-02-01 20:48:38 +0300

    overflow hidden, z-index fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 gsap.to("p", {delay: 5, duration: 2, y: "-10vw", opacity: 0, ease: "ease"});
 gsap.to(".main__arrow", {delay: 5, duration: 1.5, y: "-10vw", opacity: 0, ease: "ease"});
 
-gsap.from("form", {delay: 5, duration: 3, y: "50vw", opacity: 0, ease: "ease"});
+gsap.from("form", {delay: 5, duration: 3, y: "50vw", opacity: 0, zIndex: 1, ease: "ease"});
