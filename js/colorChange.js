b07126016ff8ea41770a6a6187fe32379ea2970f
Kirill Romanov <58993148+ronokiri2@users.noreply.github.com>
2022-02-15 14:46:51 +0300

    beautify js

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,9 +1,9 @@
 let colorWell;
-const defaultColor 	= "#EBECF0";
-const input   		= document.querySelectorAll(".input");
-const bigButton 	= document.querySelector(".big-button");
-const bottomButton 	= document.querySelectorAll(".bottom-button");
-const colorInput 	= document.querySelector(".color-input");
+const defaultColor = "#EBECF0";
+const input = document.querySelectorAll(".input");
+const bigButton = document.querySelector(".big-button");
+const bottomButton = document.querySelectorAll(".bottom-button");
+const colorInput = document.querySelector(".color-input");
 
 window.addEventListener("load", startup, false);
 
@@ -13,140 +13,144 @@ function startup() {
 	colorWell.addEventListener("input", updateFirst, false);
 	colorWell.addEventListener("change", updateAll, false);
 	colorWell.select();
-  }
+}
 
 // функция затемнения цвета
 function hexToDarkenHSL(H) {
 	// Convert hex to RGB first
-	let r = 0, g = 0, b = 0;
+	let r = 0,
+		g = 0,
+		b = 0;
 	if (H.length == 4) {
-	  r = "0x" + H[1] + H[1];
-	  g = "0x" + H[2] + H[2];
-	  b = "0x" + H[3] + H[3];
+		r = "0x" + H[1] + H[1];
+		g = "0x" + H[2] + H[2];
+		b = "0x" + H[3] + H[3];
 	} else if (H.length == 7) {
-	  r = "0x" + H[1] + H[2];
-	  g = "0x" + H[3] + H[4];
-	  b = "0x" + H[5] + H[6];
+		r = "0x" + H[1] + H[2];
+		g = "0x" + H[3] + H[4];
+		b = "0x" + H[5] + H[6];
 	}
 	// Then to HSL
 	r /= 255;
 	g /= 255;
 	b /= 255;
-	let cmin = Math.min(r,g,b),
-		cmax = Math.max(r,g,b),
+	let cmin = Math.min(r, g, b),
+		cmax = Math.max(r, g, b),
 		delta = cmax - cmin,
 		h = 0,
 		s = 0,
 		l = 0;
-  
+
 	if (delta == 0)
-	  h = 0;
+		h = 0;
 	else if (cmax == r)
-	  h = ((g - b) / delta) % 6;
+		h = ((g - b) / delta) % 6;
 	else if (cmax == g)
-	  h = (b - r) / delta + 2;
+		h = (b - r) / delta + 2;
 	else
-	  h = (r - g) / delta + 4;
-  
+		h = (r - g) / delta + 4;
+
 	h = Math.round(h * 60);
-  
+
 	if (h < 0)
-	  h += 360;
-  
+		h += 360;
+
 	l = (cmax + cmin) / 2;
 	s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 	s = +(s * 100).toFixed(1);
 	l = +(l * 100 - 18).toFixed(1);
-  
+
 	return "hsl(" + h + "," + s + "%," + l + "%)";
-  }
+}
 
 
 // функция осветления цвета
-  function hexToLighterHSL(H) {
+function hexToLighterHSL(H) {
 	// Convert hex to RGB first
-	let r = 0, g = 0, b = 0;
+	let r = 0,
+		g = 0,
+		b = 0;
 	if (H.length == 4) {
-	  r = "0x" + H[1] + H[1];
-	  g = "0x" + H[2] + H[2];
-	  b = "0x" + H[3] + H[3];
+		r = "0x" + H[1] + H[1];
+		g = "0x" + H[2] + H[2];
+		b = "0x" + H[3] + H[3];
 	} else if (H.length == 7) {
-	  r = "0x" + H[1] + H[2];
-	  g = "0x" + H[3] + H[4];
-	  b = "0x" + H[5] + H[6];
+		r = "0x" + H[1] + H[2];
+		g = "0x" + H[3] + H[4];
+		b = "0x" + H[5] + H[6];
 	}
 	// Then to HSL
 	r /= 255;
 	g /= 255;
 	b /= 255;
-	let cmin = Math.min(r,g,b),
-		cmax = Math.max(r,g,b),
+	let cmin = Math.min(r, g, b),
+		cmax = Math.max(r, g, b),
 		delta = cmax - cmin,
 		h = 0,
 		s = 0,
 		l = 0;
-  
+
 	if (delta == 0)
-	  h = 0;
+		h = 0;
 	else if (cmax == r)
-	  h = ((g - b) / delta) % 6;
+		h = ((g - b) / delta) % 6;
 	else if (cmax == g)
-	  h = (b - r) / delta + 2;
+		h = (b - r) / delta + 2;
 	else
-	  h = (r - g) / delta + 4;
-  
+		h = (r - g) / delta + 4;
+
 	h = Math.round(h * 60);
-  
+
 	if (h < 0)
-	  h += 360;
-  
+		h += 360;
+
 	l = (cmax + cmin) / 2;
 	s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 	s = +(s * 100).toFixed(1);
 	l = +(l * 100 + 7).toFixed(1);
 
 	return "hsl(" + h + "," + s + "%," + l + "%)";
-  }
+}
 
 
 
-  function updateFirst(event) {
-	let shadow 	  = hexToDarkenHSL(event.target.value);
+function updateFirst(event) {
+	let shadow = hexToDarkenHSL(event.target.value);
 	let highlight = hexToLighterHSL(event.target.value);
 
 	getComputedStyle(document.documentElement)
-	.getPropertyValue('--bg');
+		.getPropertyValue('--bg');
 
 	if (input) {
 		//смена содержимого css переменных для смены цвета
-	   document.documentElement.style
-	   .setProperty('--bg', event.target.value);
-	   document.documentElement.style
-	   .setProperty('--shadow', shadow);
-	   document.documentElement.style
-	   .setProperty('--white', highlight);
+		document.documentElement.style
+			.setProperty('--bg', event.target.value);
+		document.documentElement.style
+			.setProperty('--shadow', shadow);
+		document.documentElement.style
+			.setProperty('--white', highlight);
 
 
-	   //удаление класса transition во время смены цвета в color-input
-	   for (let i = 0; i < input.length; i++) {
-		input[i].classList.remove('transition')
-	  }
-	   bigButton.classList.remove("transition");
-	   colorInput.classList.remove("transition");
-	   for (let i = 0; i < bottomButton.length; i++) {
-		bottomButton[i].classList.remove('transition')
-	  	}
+		//удаление класса transition во время смены цвета в color-input
+		for (let i = 0; i < input.length; i++) {
+			input[i].classList.remove('transition')
+		}
+		bigButton.classList.remove("transition");
+		colorInput.classList.remove("transition");
+		for (let i = 0; i < bottomButton.length; i++) {
+			bottomButton[i].classList.remove('transition')
+		}
 	}
-  }
+}
 
 //функция добавления класса transition после окончательного выбора цвета в color-input
-  function updateAll(event) {
+function updateAll(event) {
 	for (let i = 0; i < input.length; i++) {
 		input[i].classList.add('transition')
-	  }
-	   bigButton.classList.add("transition");
-	   colorInput.classList.add("transition");
-	   for (let i = 0; i < bottomButton.length; i++) {
+	}
+	bigButton.classList.add("transition");
+	colorInput.classList.add("transition");
+	for (let i = 0; i < bottomButton.length; i++) {
 		bottomButton[i].classList.add('transition')
-	  	}
-  }
+	}
+}
